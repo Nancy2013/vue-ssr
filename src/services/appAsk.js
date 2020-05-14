f6d1d39a09b7123c932c5bf698861c19f7f7dbba
Juliette.Wang <nannan.wang@broadlink.com.cn>
2020-05-14 17:59:05 +0800

    添加services文件；

---
+++ src/services/appAsk.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-14 14:26:19
+ * @LastEditTime: 2020-05-14 14:26:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-ssr\src\services\appAsk.js
+ */
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,11 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-14 14:21:12
+ * @LastEditTime: 2020-05-14 14:31:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-ssr\src\store\modules\app.js
+ */
 import axios from 'axios';
 
 const app = {
+    namespaced:true,
     state: {
         userInfo: {},
     },
     mutations: {
-        setUserInfo(state, payload) {
+        setUserInfo(state, payload) {           
             state.userInfo = {
                 ...payload
             }
